fe624975eede98140c52577194ca1fc2f25730f9
Manéo <maneo.dangreaux@uphf.fr>
2023-05-31 12:13:15 +0200

    revenir sur la premiere page

--- app/src/main/java/com/example/projet/Supprimer.java
+++ app/src/main/java/com/example/projet/Supprimer.java
@@ -1,8 +1,10 @@
 package com.example.projet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class Supprimer extends AppCompatActivity {
 
@@ -10,6 +12,14 @@ public class Supprimer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_supprimer);
+        Toolbar t = (androidx.appcompat.widget.Toolbar) findViewById(R.id.toolbarSupprimer);
+        setSupportActionBar(t);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
 
--- app/src/main/java/com/example/projet/Terminer.java
+++ app/src/main/java/com/example/projet/Terminer.java
@@ -1,8 +1,11 @@
 package com.example.projet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class Terminer extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class Terminer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terminer);
+        Toolbar t = (androidx.appcompat.widget.Toolbar) findViewById(R.id.toolbarTerminer);
+        setSupportActionBar(t);
     }
+
 }
