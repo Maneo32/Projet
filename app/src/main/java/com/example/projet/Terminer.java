f99557f214905da484a4defc83e985639d03aadc
Baptistet21 <baptiste.tellier@uphf.fr>
2023-06-01 10:42:21 +0200

    push home terminer

--- app/src/main/java/com/example/projet/Terminer.java
+++ app/src/main/java/com/example/projet/Terminer.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class Terminer extends AppCompatActivity {
 
@@ -15,6 +18,11 @@ public class Terminer extends AppCompatActivity {
         setContentView(R.layout.activity_terminer);
         Toolbar t = (androidx.appcompat.widget.Toolbar) findViewById(R.id.toolbarTerminer);
         setSupportActionBar(t);
+    }
+
+    public void accesHome(View View) {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 
 }
