006b6bde3fea52b2475d1c94a94a22587a8f7034
Baptistet21 <baptiste.tellier@uphf.fr>
2023-06-07 11:24:25 +0200

    push btn modifier

--- app/src/main/java/com/example/projet/MainActivity.java
+++ app/src/main/java/com/example/projet/MainActivity.java
@@ -175,7 +175,9 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
+    public void accesModif(View View) {
+        Intent intent = new Intent(this,Modifier.class);
+        startActivity(intent);
+    }
 }
 
